ba32e0c7fbbe0802e89b918567e9cc0b73d49de5
Selyuu <rainierlouis@gmail.com>
2018-01-26 17:24:19 +0100

    feat: implemented create file object for user config

--- userConfig.js
+++ userConfig.js
@@ -1,6 +1,15 @@
+#! /usr/bin/env node
+const exec = require('child_process').exec;
+
+// const createFile = async (...args) => {
+//  await exec(`mkdir visual/`);
+//  await exec(`echo ${args} > visual/index.html`);
+// };
 const userConfig = {
- a: 'hello',
- myFunc: () => `${userConfig.a}!!!!!`
+ createFile: async entry => {
+  await exec(`mkdir visual/`);
+  await exec(`echo ${entry} > visual/index.txt`);
+ }
 };
 
 module.exports = {
