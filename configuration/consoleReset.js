e054bab8163f37584a3a54d7d74cf38146dad0e1
Selyuu <rainierlouis@gmail.com>
2018-01-27 14:58:44 +0100

    style: implemented chalk coloring

--- configuration/consoleReset.js
+++ configuration/consoleReset.js
@@ -1,11 +1,17 @@
 const readline = require('readline');
+const chalk = require('chalk');
 
 const reset = {
  reset: () => process.stdout.write('\x1B[2J\x1B[0f'),
- percent: p => {
+ resetTick: (tick, space = '') => {
+  return process.stdout.write(`${chalk.grey(tick)}${space}`);
+ },
+ percent: (p, t, b = false, c) => {
   readline.cursorTo(process.stdout, 0);
-  process.stdout.write(`Clearing Visual data --`);
-  process.stdout.write(`Clearing ... ${p}%`);
+  reset.resetTick(t);
+  reset.resetTick(t, '    ');
+  process.stdout.write(`${chalk.green('Cleaning')} ... ${chalk[c](`${p} %`)}`);
+  b ? readline.cursorTo(process.stdout, 0) : null;
  }
 };
 
