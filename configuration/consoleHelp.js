745952144fe84c35ca301f744f488d368455e4a2
Selyuu <rainierlouis@gmail.com>
2018-01-27 13:05:43 +0100

    feat: implemented help module

---
+++ configuration/consoleHelp.js
@@ -0,0 +1,28 @@
+const chalk = require('chalk');
+
+const help = {
+ helperData: `${chalk.bold.blue('"OVERVIEW"')} - ${chalk.dim(
+  'creates a visual file for your application structure'
+ )}
+
+	${chalk.dim('Usage')}
+		${chalk.green(
+   '$ ov <entry>'
+  )} Declare entry point for the visualisation ${chalk.red.bold('required')}
+
+	${chalk.dim('Options')}
+		${chalk.green('<full|single>')} ${chalk.cyan(
+  '[-f -s]'
+ )} Declare what to visualise, full structure by default
+
+	${chalk.dim('Help')}
+		${chalk.green('<help>')} ${chalk.cyan('[-h]')} Further detailing on options
+
+	${chalk.dim('Reset')}
+		${chalk.green('<reset>')} ${chalk.cyan('[-r]')} Reset + delete visual folder
+	`
+};
+
+module.exports = {
+ help
+};
