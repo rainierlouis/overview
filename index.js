2651c9d2ffb31e661c26ca111d4efa8c52db6b5a
Selyuu <rainierlouis@gmail.com>
2018-01-27 17:32:20 +0100

    feat: implemented start flag

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const patternSearch = userArgs[0] || [];
 //-- MODULES --//
 const { reset, resetEntire } = require('./configuration/consoleReset');
 const { menu } = require('./configuration/consoleHelp');
-const { createUserConf } = require('./configuration/userConfig');
+const { create, missingEntry } = require('./configuration/userConfig');
 //-------------//
 
 //-- PACKAGES --//
@@ -22,21 +22,28 @@ ${chalk.bold.blue('"OVERVIEW"')} - ${chalk.dim(
   'creates a visual file for your application structure'
  )}
 
-${chalk.dim('Usage')}
+${chalk.dim('Entry')}
 	${chalk.green(
   '$ ov <entry>'
  )} Declare entry point for the visualisation ${chalk.red.bold('required')}
+
+	${chalk.dim('Begin')}
+		${chalk.green('<begin>')} ${chalk.cyan(
+  '[-b]'
+ )} Once entry is declared, you can start the visual creation
 
 ${chalk.dim('Options')}
 	${chalk.green('<full|single>')} ${chalk.cyan(
   '[-f -s]'
  )} Declare what to visualise, full structure by default
 
+	${chalk.dim('Reset')}
+		${chalk.green('<reset>')} ${chalk.cyan('[-r]')} Reset + delete visual folder
+
 ${chalk.dim('Help')}
 	${chalk.green('<help>')} ${chalk.cyan('[-h]')} Further detailing on options
 
-${chalk.dim('Reset')}
-	${chalk.green('<reset>')} ${chalk.cyan('[-r]')} Reset + delete visual folder
+
 `,
  {
   flags: {
@@ -51,6 +58,12 @@ ${chalk.dim('Reset')}
    reset: {
     type: 'boolean',
     alias: 'r'
+   },
+   help: {
+    alias: 'h'
+   },
+   start: {
+    alias: 'b'
    }
   }
  },
@@ -58,7 +71,9 @@ ${chalk.dim('Reset')}
   alias: {
    f: 'full',
    s: 'single',
-   r: 'reset'
+   r: 'reset',
+   h: 'help',
+   b: 'begin'
   },
   default: {
    name: 'App.js',
@@ -70,25 +85,38 @@ ${chalk.dim('Reset')}
 );
 
 const ov = async (input, flags) => {
- const keys = await Object.keys(flags);
+ const keys = await Object.keys(flags).filter(
+  el => el === 'h' || el === 'r' || el === 's' || el === 'f' || el === 'b'
+ );
+ if (!input && keys.length === 0) {
+  await reset();
+  await log(missingEntry());
+ } else if (input && keys.length === 0) {
+  await reset();
+  // await create(input);
+ }
+
  keys.forEach(async el => {
   switch (el) {
-   case 'h': // help
-    await consReset.reset();
-    await log(consHelp.helperData);
+   case 'h':
+    await reset();
+    await log(menu);
     break;
-   case 'r': // reset
-    resetEntire();
+   case 'r':
+    await resetEntire();
     break;
-   case 's': // single traversal
-    // await consReset.reset();
+   case 's':
     await log('s flag!');
     break;
-   case 'f': // full traversal
-    // await consReset.reset();
-    await createUserConf(input);
+   case 'f':
+    await reset();
+    await create(input);
+    break;
+   case 'b':
+    log('start flag wooooh');
     break;
    default:
+    break;
   }
  });
 };
