c3041f7dedd83b87056edca4478509dddbdf159f
Selyuu <rainierlouis@gmail.com>
2018-01-27 19:00:38 +0100

    feat: remove folder + file method implemented

---
+++ configuration/consoleRemove.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const readline = require('readline');
+
+const folderPath = path.join(__dirname, '../Visual');
+
+const remove = {
+ remDir: async () => {
+  await fs.rmdir(folderPath, err => {
+   if (err) throw err;
+  });
+ },
+ remFile: async () => {
+  await fs.unlink(`${folderPath}/user.config.js`, err => {
+   if (err) throw err;
+
+   remove.remDir();
+  });
+ }
+};
+
+module.exports = {
+ remove: remove.remFile
+};
