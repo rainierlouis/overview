74cf3de4fe71894e4ecab349018ee0e519e3f047
Selyuu <rainierlouis@gmail.com>
2018-01-27 13:04:43 +0100

    refactor: switch cases for user flags

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const userArgs = process.argv.slice(2);
 const patternSearch = userArgs[0] || [];
 
-const userConf = require('./userConfig'); // userConfig -- create user.config
+const consReset = require('./configuration/consoleReset').reset; // consoleReset -- clear console + reset cursor
+const consHelp = require('./configuration/consoleHelp').help; // consoleHelp -- provide help data on terminal
+const userConf = require('./configuration/userConfig'); // userConfig -- create user.config
 const user = userConf.userConfig;
+
+const log = console.log;
 
 const woofwoof = require('woofwoof');
 const chalk = require('chalk');
+const readline = require('readline');
 
 const cli = woofwoof(
  `
@@ -39,31 +44,60 @@ ${chalk.dim('Reset')}
    single: {
     type: 'boolean',
     alias: 's'
+   },
+   reset: {
+    type: 'boolean',
+    alias: 'r'
    }
   }
  },
  {
   alias: {
    f: 'full',
-   s: 'single'
+   s: 'single',
+   r: 'reset'
   },
   default: {
    name: 'App.js',
    full: true,
-   single: false
+   single: false,
+   reset: false
   }
  }
 );
 
-const ov = (input, flags) => {
- if (Object.keys(flags).filter(el => el === 's').length > 0) {
-  // do -s flag
-  console.log('s flag!');
- } else {
-  // do default
-  console.log('default!');
-  user.createFile(input);
- }
+function writeWaitingPercent(p) {
+ //readline.clearLine(process.stdout);
+ readline.cursorTo(process.stdout, 0);
+ process.stdout.write(`waiting ... ${p}%`);
+}
+
+const ov = async (input, flags) => {
+ const keys = await Object.keys(flags);
+ keys.forEach(async el => {
+  switch (el) {
+   case 'h': // help
+    await consReset.reset();
+    await log(consHelp.helperData);
+    break;
+   case 'r': // reset
+    await setTimeout(() => consReset.percent(0), 0);
+    await setTimeout(() => consReset.percent(33), 1500);
+    await setTimeout(() => consReset.percent(66), 3000);
+    await setTimeout(() => consReset.percent(99), 4500);
+    break;
+   case 's': // single traversal
+    // await consReset.reset();
+    await log('s flag!');
+    break;
+   case 'f': // full traversal
+    // await consReset.reset();
+    await log('f flag!');
+    // await user.createFile(input);
+    break;
+   default:
+  }
+ });
 };
 
 ov(cli.input[0], cli.flags);
