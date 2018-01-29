da109b1e6f4ced3856750eb0f61989e6092ec2fe
Selyuu <rainierlouis@gmail.com>
2018-01-29 11:20:50 +0100

    feat: implemented deletion for visual html file

--- configuration/consoleRemove.js
+++ configuration/consoleRemove.js
@@ -1,25 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
-const readline = require('readline');
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const readline = require("readline");
 
-const folderPath = path.join(__dirname, '../Visual');
+const folderPath = path.join(__dirname, "../Visual");
 
 const remove = {
- remDir: async () => {
-  await fs.rmdir(folderPath, err => {
-   if (err) throw err;
-  });
- },
- remFile: async () => {
-  await fs.unlink(`${folderPath}/user.config.js`, err => {
-   if (err) throw err;
-
-   remove.remDir();
-  });
- }
+  remDir: async () => {
+    await fs.rmdir(folderPath, err => {
+      if (err) throw err;
+    });
+  },
+  remFile: async () => {
+    await fs.unlink(`${folderPath}/user.config.js`, err => {
+      if (err) throw err;
+    });
+    // -- delete visual.index -- //
+    // await fs.unlick(`${folderPath}/visual.html`, err => {
+    //  if (err) throw err;
+    // });
+    await remove.remDir();
+  }
 };
 
 module.exports = {
- remove: remove.remFile
+  remove: remove.remFile
 };
