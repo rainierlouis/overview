16d1f64c08c5b867f64c123e21471b2ca4f3d870
Selyuu <rainierlouis@gmail.com>
2018-01-30 14:27:19 +0100

    feat: implemented ascii header

--- configuration/consoleHelp.js
+++ configuration/consoleHelp.js
@@ -1,40 +1,38 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
+
+const asciimo = require("../node_modules/asciimo/lib/asciimo").Figlet;
+const colors = require("../node_modules/asciimo/lib/colors");
+
+const log = console.log;
 
 const help = {
- menu: `
+  menu: async () => {
+    await asciimo.write("Overview", "isometric1", async art => {
+      await log(art.rainbow);
+      await log(`
 
-	${chalk.bold.blue('"OVERVIEW"')} - ${chalk.dim(
-  'creates a visual file for your application structure'
- )}
+	${chalk.dim("creates a visual file for your application structure - v0.3.0")}
 
 
-	${chalk.dim('Entry')}
+	${chalk.dim("Entry")}
 		${chalk.green(
-   '$ ov <entry>'
-  )} Declare entry point for the visualisation ${chalk.red.bold(
-  'required first'
- )}
+      "$ overview <entry>"
+    )} Declare entry point for the visualisation ${chalk.red.bold(
+        "required first"
+      )}
 
-	${chalk.dim('Begin')}
-		${chalk.green('<begin>')} ${chalk.cyan(
-  '[-b]'
- )} Once entry is declared, you can start the visual creation
+	${chalk.dim("Reset")}
+		${chalk.green("<reset>")} ${chalk.cyan("[-r]")} Reset + delete visual folder
 
-	${chalk.dim('Options')}
-		${chalk.green('<full|single>')} ${chalk.cyan(
-  '[-f -s]'
- )} Declare what to visualise, ${chalk.red('-f')} by default
-
-	${chalk.dim('Reset')}
-		${chalk.green('<reset>')} ${chalk.cyan('[-r]')} Reset + delete visual folder
-
-	${chalk.dim('Help')}
-		${chalk.green('<help>')} ${chalk.cyan('[-h]')} Further detailing on options
+	${chalk.dim("Help")}
+		${chalk.green("<help>")} ${chalk.cyan("[-h]")} Further detailing on options
 
 
-	`
+  	`);
+    });
+  }
 };
 
 module.exports = {
- menu: help.menu
+  menu: help.menu
 };
