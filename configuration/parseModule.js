00a1a74f4a4b90df34f7bb0bdada20c118f15334
Selyuu <rainierlouis@gmail.com>
2018-01-31 16:03:47 +0100

    feat: implemented parse logic + data transfer

--- configuration/parseModule.js
+++ configuration/parseModule.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const parse = require("../parser/parse");
+
+// TODO: Require parser function, invoke
+// TODO: Take parsed data, create folder/file in client
+
+const dataContent = {};
+
+const parsing = async entryPoint => {
+  const filePath = await path.join(__dirname, `../${entryPoint}`);
+  return await parse(filePath);
+
+  console.log("FILE PATH MY NINJA", filePath);
+};
+
+module.exports = { parsing };
