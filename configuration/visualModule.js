34a4017482850915c12e6af61adda984d1228673
Selyuu <rainierlouis@gmail.com>
2018-02-02 13:05:55 +0100

    refactor: removed commented code

--- configuration/visualModule.js
+++ configuration/visualModule.js
@@ -3,13 +3,11 @@ const path = require("path");
 const mkdirp = require("mkdirp");
 
 const visualData = async pwd => {
-  // create folder
   await mkdirp(`visual`, err => {
     if (err) throw err;
   });
-  // copy folder
+
   await fs.copy("./node_modules/app-overview/client", `visual`);
-  // paste folder
 };
 
 module.exports = { visualData };
