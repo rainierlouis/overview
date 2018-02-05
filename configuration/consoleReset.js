fe418a2facfe90d0a81b4a8f22b35f146b62fb85
Selyuu <rainierlouis@gmail.com>
2018-02-05 14:43:57 +0100

    refactor: exported entire object module

--- configuration/consoleReset.js
+++ configuration/consoleReset.js
@@ -1,37 +1,20 @@
 const readline = require("readline");
 const chalk = require("chalk");
+const Ora = require("ora");
 
-const { menu } = require("./consoleHelp");
-const { remove } = require("./consoleRemove");
+const menu = require("./consoleHelp").menu;
+const remove = require("./consoleRemove").remFile;
 
 const log = console.log;
 
-const reset = {
-  // test comment
+module.exports = {
   reset: () => process.stdout.write("\x1B[2J\x1B[0f"),
-  resetTick: (tick, space = "") => {
-    return process.stdout.write(`${chalk.green.cyan(tick)}${space}`);
+  loadSpinner: async () => {
+    const spinner = new Ora({});
   },
-  percent: (p, t, b = false, c) => {
-    readline.cursorTo(process.stdout, 0);
-    reset.resetTick(`[ ${t}`);
-    reset.resetTick(`${t} ]     `);
-    process.stdout.write(
-      `${chalk.green("Cleaning")} ... ${chalk[c](`${p} %`)}`
-    );
-    b ? readline.cursorTo(process.stdout, 0) : null;
-  },
-  timerFunc: (func, time) => setTimeout(func, time),
-  resetMethod: async () => {
+  resetMethod: async pathD => {
     await reset.reset();
-    await reset.timerFunc(() => reset.percent(0, "|", false, "red"), 0);
-    await reset.timerFunc(() => reset.percent(21, "/", false, "red"), 500);
-    await reset.timerFunc(() => reset.percent(43, "|", false, "yellow"), 1000);
-    await reset.timerFunc(() => reset.percent(65, "\\", false, "yellow"), 1500);
-    await reset.timerFunc(() => reset.percent(87, "|", false, "green"), 2000);
     // await remove();
-    await reset.timerFunc(() => reset.percent(99, "/", false, "green"), 2500);
-    await reset.timerFunc(() => reset.percent(100, "|", true, "green"), 3000);
     await reset.timerFunc(() => {
       reset.reset();
       log(
@@ -44,10 +27,4 @@ const reset = {
       menu();
     }, 3500);
   }
-};
-
-module.exports = {
-  reset: reset.reset,
-  resetEntire: reset.resetMethod,
-  tick: reset.resetTick
 };
