d17e75ee61334bf2e60219ece897649cbd10ae1d
Selyuu <rainierlouis@gmail.com>
2018-02-05 19:42:29 +0100

    refactor: changed text when visual is complete

--- configuration/userConfig.js
+++ configuration/userConfig.js
@@ -31,7 +31,7 @@ const invalidLog = `
 
 	${chalk.red.bold(
     "ERR - Invalid input or flag:"
-  )}\n\n 	An entry point is ${chalk.green(
+  )}\n\n 	A valid entry point is ${chalk.green(
   "required"
 )} - Please input one and try again. ${chalk.grey(
   "(e.g. overview App.js)"
@@ -57,6 +57,10 @@ module.exports = {
   checkNodeModules: pwd => {
     pathCheck = `${pwd}/node_modules`;
     return fs.existsSync(pathCheck);
+  },
+  checkEntryPoint: (pwd, entryPoint) => {
+    entryPathCheck = `${pwd}/${entryPoint}`;
+    return fs.existsSync(entryPathCheck);
   },
   loadSpinner: async () => {
     const spinner = new Ora({});
@@ -97,7 +101,9 @@ module.exports = {
     setTimeout(() => {
       spinner.succeed(" Creating visual folder");
       log(`
- Please open ${chalk.cyan("visual/overview.html")} in your preferred browser ✌️
+ Visual file has been created at ${chalk.cyan(
+   "visual/overview.html"
+ )} - Enjoy! ✌️
 
 								  				`);
     }, 6000);
