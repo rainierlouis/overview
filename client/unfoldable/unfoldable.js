98ec062794cdde1baaae6dfbd189310c23622e6d
Achim Bode <joachim.bode@data-farms.com>
2018-02-05 19:44:10 +0100

    refactor: externalise search into an external search.js (working for tree so far only)

--- client/unfoldable/unfoldable.js
+++ client/unfoldable/unfoldable.js
@@ -61,52 +61,52 @@ function unfoldToLevel(root, toLevel) {
   });
 }
 
-function findNodes(name) {
-  caseSensitivity = false;
-  if (!caseSensitivity) name = name.toLowerCase();
-  var found = [];
-  traverse(root, function(node) {
-    var nodename = node.data.name;
-    if (!caseSensitivity) nodename = nodename.toLowerCase();
-    if (nodename.search(name) !== -1) {
-      found.push(node);
-    }
-  });
-  return found;
-}
-
-function showSearch() {
-  let jqSearch = $(`
-    <div class="graphsearch">
-      <input placeholder="hello text"/>
-    </div>
-    `);
-  $(jqSearch).insertAfter("#switch");
-  $(".graphsearch input").on("input", function(e) {
-    let inputText = $(e.delegateTarget).val();
-    if (inputText.length > 0) {
-      let arrFoundNodes = findNodes(inputText);
-      highlight(arrFoundNodes);
-      // console.log(arrFoundNodes);
-    } else removeHighlights();
-  });
-}
-
-function highlight(arrNodes) {
-  removeHighlights();
-  arrNodes.forEach(function(node) {
-    node.highlight = 1;
-    update(node);
-  });
-}
-
-function removeHighlights(exepArrNodes, doUpdate) {
-  traverse(root, function(node) {
-    delete node.highlight;
-    update(node);
-  });
-}
-
+// function findNodes(name) {
+//   caseSensitivity = false;
+//   if (!caseSensitivity) name = name.toLowerCase();
+//   var found = [];
+//   traverse(root, function(node) {
+//     var nodename = node.data.name;
+//     if (!caseSensitivity) nodename = nodename.toLowerCase();
+//     if (nodename.search(name) !== -1) {
+//       found.push(node);
+//     }
+//   });
+//   return found;
+// }
+//
+// function showSearch() {
+//   let jqSearch = $(`
+//     <div class="graphsearch">
+//       <input placeholder="hello text"/>
+//     </div>
+//     `);
+//   $(jqSearch).insertAfter("#switch");
+//   $(".graphsearch input").on("input", function(e) {
+//     let inputText = $(e.delegateTarget).val();
+//     if (inputText.length > 0) {
+//       let arrFoundNodes = findNodes(inputText);
+//       highlight(arrFoundNodes);
+//       // console.log(arrFoundNodes);
+//     } else removeHighlights();
+//   });
+// }
+//
+// function highlight(arrNodes) {
+//   removeHighlights();
+//   arrNodes.forEach(function(node) {
+//     node.highlight = 1;
+//     update(node);
+//   });
+// }
+//
+// function removeHighlights(exepArrNodes, doUpdate) {
+//   traverse(root, function(node) {
+//     delete node.highlight;
+//     update(node);
+//   });
+// }
+//
 // Collapse the node and all it's children
 function collapse(d) {
   if (d.children) {
