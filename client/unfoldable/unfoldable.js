27f558be725c32a74b357443d9449561d461caff
Achim Bode <joachim.bode@data-farms.com>
2018-02-06 12:44:32 +0100

    refactor: create class Unfoldable to be able to act on instances of different animation from shared code - CAUTION: NOT WORKING YET

--- client/unfoldable/unfoldable.js
+++ client/unfoldable/unfoldable.js
@@ -1,39 +1,3 @@
-function convertJson(data) {
-  let id = typeof data.root == "string" ? data.root : "root";
-  let processed = [];
-  let depth = 0;
-
-  function replaceObjectIdsByActualObjects(id, data, processed, depth) {
-    let dataObjectId = id;
-    let alreadyProcessed = processed.indexOf(id) != -1;
-    processed.push(dataObjectId);
-    let actualDataObject = data[dataObjectId];
-    let newChildrenArray;
-    let childrenSupressedToAvoidRecursion;
-    if (!alreadyProcessed) {
-      newChildrenArray = actualDataObject.children.map(function(childId) {
-        return replaceObjectIdsByActualObjects(
-          childId,
-          data,
-          processed,
-          depth + 1
-        );
-      });
-      childrenSupressedToAvoidRecursion = false;
-    } else {
-      childrenSupressedToAvoidRecursion = actualDataObject.children.length > 0;
-      newChildrenArray = [];
-    }
-    let copyOfObjectWithNewChildren = Object.assign({}, actualDataObject, {
-      children: newChildrenArray
-    });
-    return copyOfObjectWithNewChildren;
-  }
-
-  let result = replaceObjectIdsByActualObjects(id, data, processed, depth);
-  return result;
-}
-
 function unmountTree() {
   // @TODO: look for things to clean up and tidy them here
 }
@@ -51,79 +15,24 @@ function showSearch() {
 }
 
 function mountTree() {
-  loadedTreeData = data; // store old object
-  console.log(loadedTreeData);
-  var treeData = convertJson(loadedTreeData);
-  console.log(treeData);
-  // Set the dimensions and margins of the diagram
-  var margin = { top: 20, right: 90, bottom: 30, left: 90 },
-    width = $("#graph").innerWidth() - margin.left - margin.right,
-    height = $("#graph").innerHeight() - margin.top - margin.bottom;
+  function Unfoldable(data) {
+    this.loadedTreeData = data; // store old object
+    console.log(this.loadedTreeData);
+    this.treeData = this.convertJson(this.loadedTreeData);
+    console.log(this.treeData);
 
-  // append the svg object to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
-  var svg = d3v4
-    .select("#graph")
-    .append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var i = 0,
-    duration = 750,
-    root;
-
-  // declares a tree layout and assigns the size
-  var treemap = d3v4.tree().size([height, width]);
-
-  // Assigns parent, children, height, depth
-  root = d3v4.hierarchy(treeData, function(d) {
-    return d.children;
-  });
-  root.x0 = height / 2;
-  root.y0 = 0;
-
-  // Collapse after the second level
-  // root.children.forEach(collapse);
-  unfoldToLevel(root, 3);
-
-  function traverse(node, func) {
-    let goDeeper = func(node);
-    let toBeTraversed = node["children"] || node["_children"] || [];
-    if (goDeeper !== false)
-      toBeTraversed.forEach(function(node) {
-        traverse(node, func);
-      });
-  }
-
-  function unfoldToLevel(root, toLevel) {
-    traverse(root, function(node) {
-      let hasUnfoldDepth = node.depth < toLevel;
-      if (hasUnfoldDepth) {
-        if (!node["children"]) node["children"] = node["_children"];
-      } else {
-        collapse(node);
-        return false;
-      }
+    this.init(this.treeData);
+    // Collapse after the second level
+    var thisref = this;
+    this.root.children.forEach(function(child, i) {
+      thisref.collapse.call(thisref, child);
     });
+    // unfoldToLevel(root, 3);
   }
 
-  update(root);
-
-  // Collapse the node and all it's children
-  function collapse(d) {
-    if (d.children) {
-      d._children = d.children;
-      d._children.forEach(collapse);
-      d.children = null;
-    }
-  }
-
-  function update(source) {
+  Unfoldable.prototype.update = function(source) {
     // Assigns the x and y position for the nodes
-    var treeData = treemap(root);
+    var treeData = this.treemap(this.root);
 
     // Compute the new tree layout.
     var nodes = treeData.descendants(),
@@ -137,8 +46,8 @@ function mountTree() {
     // ****************** Nodes section ***************************
 
     // Update the nodes...
-    var node = svg.selectAll("g.node").data(nodes, function(d) {
-      return d.id || (d.id = ++i);
+    var node = this.svg.selectAll("g.node").data(nodes, function(d) {
+      return d.id || (d.id = ++this.i);
     });
 
     // Enter any new modes at the parent's previous position.
@@ -180,7 +89,7 @@ function mountTree() {
     // Transition to the proper position for the node
     nodeUpdate
       .transition()
-      .duration(duration)
+      .duration(this.duration)
       .attr("transform", function(d) {
         return "translate(" + d.y + "," + d.x + ")";
       });
@@ -198,7 +107,7 @@ function mountTree() {
     var nodeExit = node
       .exit()
       .transition()
-      .duration(duration)
+      .duration(this.duration)
       .attr("transform", function(d) {
         return "translate(" + source.y + "," + source.x + ")";
       })
@@ -213,7 +122,7 @@ function mountTree() {
     // ****************** links section ***************************
 
     // Update the links...
-    var link = svg.selectAll("path.link").data(links, function(d) {
+    var link = this.svg.selectAll("path.link").data(links, function(d) {
       return d.id;
     });
 
@@ -224,7 +133,8 @@ function mountTree() {
       .attr("class", "link")
       .attr("d", function(d) {
         var o = { x: source.x0, y: source.y0 };
-        return diagonal(o, o);
+        var diag = diagonal(o, o);
+        return diag;
       });
 
     // UPDATE
@@ -233,7 +143,7 @@ function mountTree() {
     // Transition back to the parent element position
     linkUpdate
       .transition()
-      .duration(duration)
+      .duration(this.duration)
       .attr("d", function(d) {
         return diagonal(d, d.parent);
       });
@@ -242,7 +152,7 @@ function mountTree() {
     var linkExit = link
       .exit()
       .transition()
-      .duration(duration)
+      .duration(this.duration)
       .attr("d", function(d) {
         var o = { x: source.x, y: source.y };
         return diagonal(o, o);
@@ -257,7 +167,7 @@ function mountTree() {
 
     // Creates a curved (diagonal) path from parent to the child nodes
     function diagonal(s, d) {
-      path = `M ${s.y} ${s.x}
+      let path = `M ${s.y} ${s.x}
       C ${(s.y + d.y) / 2} ${s.x},
       ${(s.y + d.y) / 2} ${d.x},
       ${d.y} ${d.x}`;
@@ -274,9 +184,116 @@ function mountTree() {
         d.children = d._children;
         d._children = null;
       }
-      update(d);
+      this.update(d);
     }
-  }
+  };
+
+  Unfoldable.prototype.init = function(data) {
+    // Set the dimensions and margins of the diagram
+    (this.margin = { top: 20, right: 90, bottom: 30, left: 90 }),
+      (this.width =
+        $("#graph").innerWidth() - this.margin.left - this.margin.right),
+      (this.height =
+        $("#graph").innerHeight() - this.margin.top - this.margin.bottom);
+
+    // append the svg object to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    this.svg = d3v4
+      .select("#graph")
+      .append("svg")
+      .attr("width", this.width + this.margin.right + this.margin.left)
+      .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")"
+      );
+
+    this.i = 0;
+    this.duration = 750;
+
+    // declares a tree layout and assigns the size
+    this.treemap = d3v4.tree().size([this.height, this.width]);
+
+    // Assigns parent, children, height, depth
+    this.root = d3v4.hierarchy(this.treeData, function(d) {
+      return d.children;
+    });
+    this.root.x0 = this.height / 2;
+    this.root.y0 = 0;
+
+    this.update(this.root);
+  };
+
+  Unfoldable.prototype.convertJson = function(data) {
+    let id = typeof data.root == "string" ? data.root : "root";
+    let processed = [];
+    let depth = 0;
+
+    function replaceObjectIdsByActualObjects(id, data, processed, depth) {
+      let dataObjectId = id;
+      let alreadyProcessed = processed.indexOf(id) != -1;
+      processed.push(dataObjectId);
+      let actualDataObject = data[dataObjectId];
+      let newChildrenArray;
+      let childrenSupressedToAvoidRecursion;
+      if (!alreadyProcessed) {
+        newChildrenArray = actualDataObject.children.map(function(childId) {
+          return replaceObjectIdsByActualObjects(
+            childId,
+            data,
+            processed,
+            depth + 1
+          );
+        });
+        childrenSupressedToAvoidRecursion = false;
+      } else {
+        childrenSupressedToAvoidRecursion =
+          actualDataObject.children.length > 0;
+        newChildrenArray = [];
+      }
+      let copyOfObjectWithNewChildren = Object.assign({}, actualDataObject, {
+        children: newChildrenArray
+      });
+      return copyOfObjectWithNewChildren;
+    }
+
+    let result = replaceObjectIdsByActualObjects(id, data, processed, depth);
+    return result;
+  };
+
+  Unfoldable.prototype.traverse = function(node, func) {
+    let goDeeper = func(node);
+    let toBeTraversed = node["children"] || node["_children"] || [];
+    if (goDeeper !== false)
+      toBeTraversed.forEach(function(node) {
+        traverse(node, func);
+      });
+  };
+
+  Unfoldable.prototype.unfoldToLevel = function(root, toLevel) {
+    traverse(root, function(node) {
+      let hasUnfoldDepth = node.depth < toLevel;
+      if (hasUnfoldDepth) {
+        if (!node["children"]) node["children"] = node["_children"];
+      } else {
+        this.collapse(node);
+        return false;
+      }
+    });
+  };
+
+  // Collapse the node and all it's children
+  Unfoldable.prototype.collapse = function(d) {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(arguments.callee);
+      d.children = null;
+    }
+  };
+
+  var unfoldable = new Unfoldable(data);
 }
 
 // window.onload = function() {
@@ -285,6 +302,7 @@ function mountTree() {
 (function($) {
   $(document).ready(function() {
     if (typeof switch_is_present == "undefined") mountTree();
-    showSearch();
+    // showSearch();
   });
 })(jQuery);
+//
