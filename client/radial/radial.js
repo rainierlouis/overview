dbdeedda30abe04c17aef4c12fa1eb3dcded36b1
palevoo <kud.nikita@gmail.com>
2018-02-07 18:12:48 +0100

    feat: nice refactor, cleaned codebase

--- client/radial/radial.js
+++ client/radial/radial.js
@@ -6,11 +6,6 @@ function mountRadial() {
   let tree;
   let linkedNodes = {};
 
-  // const force = d3v3.layout.force()//d3v4.forceSimulation
-  //   .charge(-800)//d3v4.forceManyBody() >>>> // manyBody.strength([strength]) // manyBody.theta([theta]) // manyBody.distanceMin([distance]) // manyBody.distanceMax([distance])
-  //   .size([width, height]); //x.x & y.y
-
-  //V4
   let svg = d3v4.select("#graph")
     .append("svg")
     .attr("width", width)
@@ -21,16 +16,6 @@ function mountRadial() {
       svg.attr("transform", d3v4.event.transform)
     }))
     .append("g")
-
-  // let svg = d3v3.select("#graph").append("svg")
-  //   .attr("width", width)
-  //   .attr("height", height)
-  //   .call(d3v3.behavior.zoom() // d3v4.zoom()
-  //   .scaleExtent([0.4, 4])
-  //   .on("zoom", function () {
-  //     svg.attr("transform", "translate(" + d3v3.event.translate + ")" + " scale(" + d3v3.event.scale + ")")
-  //   })) // event.tranform.x && event.transform.y // event.transform.k
-  //   .append("g")
 
   const render = (data) => {
     let arr = [];
@@ -47,14 +32,12 @@ function mountRadial() {
     links: getLinks(render(data))
   };
 
-  //V4
   var simulation = d3v4.forceSimulation(tree.nodes)
-      .force("charge", d3v4.forceManyBody())
-      .force("collide",d3v4.forceCollide(15))
-      .force("link", d3v4.forceLink(tree.links).strength(0.1))
-      .force("center", d3v4.forceCenter(width / 2, height / 2))
-      .on("tick", ticked);
-
+    .force("charge", d3v4.forceManyBody())
+    .force("collide",d3v4.forceCollide(15))
+    .force("link", d3v4.forceLink(tree.links).strength(0.1))
+    .force("center", d3v4.forceCenter(width / 2, height / 2))
+    .on("tick", ticked);
 
   var link = svg.append("g")
     .attr("class", "links")
@@ -87,9 +70,6 @@ function mountRadial() {
       .attr('x', 8)
       .attr('y', 18);
 
-  node.append("title")
-      .text(function(d) { return d.name; });
-
   simulation
       .nodes(tree.nodes)
       .alphaTarget(1).restart()
@@ -100,15 +80,15 @@ function mountRadial() {
 
   function ticked() {
     link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
     node
-        .attr("transform", function(d) {
-          return "translate(" + d.x + "," + d.y + ")";
-        })
+      .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      })
   }
 
   function dragstarted(d) {
@@ -137,30 +117,29 @@ function mountRadial() {
   };
 
   function mouseOver(opacity) {
-      return function(d) {
-
-          node.style("stroke-opacity", function(o) {
-              let thisOpacity = isConnected(d, o) ? 1 : opacity;
-              return thisOpacity;
-          });
-          node.style("fill-opacity", function(o) {
-              let thisOpacity = isConnected(d, o) ? 1 : opacity;
-              return thisOpacity;
-          });
-          link.style("stroke-opacity", function(o) {
-              return o.source === d || o.target === d ? 1 : opacity;
-          });
-          link.style("stroke", function(o){
-              return o.source === d || o.target === d ? o.source.colour : "#ddd";
-          });
-      };
+    return function(d) {
+      node.style("stroke-opacity", function(o) {
+          let thisOpacity = isConnected(d, o) ? 1 : opacity;
+          return thisOpacity;
+      });
+      node.style("fill-opacity", function(o) {
+          let thisOpacity = isConnected(d, o) ? 1 : opacity;
+          return thisOpacity;
+      });
+      link.style("stroke-opacity", function(o) {
+          return o.source === d || o.target === d ? 1 : opacity;
+      });
+      link.style("stroke", function(o){
+          return o.source === d || o.target === d ? o.source.colour : "#ddd";
+      });
+    };
   };
 
   function mouseOut() {
-      node.style("stroke-opacity", 1);
-      node.style("fill-opacity", 1);
-      link.style("stroke-opacity", 1);
-      link.style("stroke", "#ddd");
+    node.style("stroke-opacity", 1);
+    node.style("fill-opacity", 1);
+    link.style("stroke-opacity", 1);
+    link.style("stroke", "#ddd");
   };
 
   function getLinks(data) {
